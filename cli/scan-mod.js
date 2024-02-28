597cce8d28295645e61bc7b25a8b4a5dca0197f0
Dmitry Agapov <dmitryagapov89@gmail.com>
2024-02-28 20:58:56 +0300

    Initial implementation

---
+++ cli/scan-mod.js
@@ -0,0 +1,88 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import url from 'node:url';
+import * as xml2js from 'xml2js';
+import * as commander from 'commander';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+async function main() {
+    const program = commander.program
+        .argument('<string>', 'Path to mod folder')
+        .option('-o [string]', 'Output file path', path.resolve(__dirname, '../mods.json'))
+        .action(async (path, { o }) => {
+            const modsIndex = await scanMod(path);
+
+            await writeModsIndex(modsIndex, o);
+            console.log('Done!');
+        });
+
+    await program.parseAsync();
+}
+
+async function scanMod(dir) {
+    dir = path.join(dir, 'Patches');
+
+    const dirContent = await fs.readdir(dir);
+    const result = {};
+
+    console.log(`Scanning ${dirContent.length} directories`);
+
+    for (const dirName of dirContent) {
+        const modNames = await scanPatchSubdir(path.join(dir, dirName));
+
+        result[dirName] = modNames;
+    }
+
+    return result;
+}
+
+async function scanPatchSubdir(dir) {
+    const dirContent = await fs.readdir(dir, { recursive: true });
+    const result = new Set();
+
+    for (const fileOrDir of dirContent) {
+        if (fileOrDir.toLowerCase().endsWith('.xml')) {
+            const modNames = await scanPatchFile(path.join(dir, fileOrDir));
+
+            modNames.forEach(result.add, result)
+        }
+    }
+
+    return result;
+}
+
+async function scanPatchFile(path) {
+    const file = await fs.readFile(path, { encoding: 'utf8' });
+    const obj = await xml2js.parseStringPromise(file);
+    const result = new Set();
+
+    if (!Array.isArray(obj.Patch.Operation)) {
+        return result;
+    }
+
+    for (const lvl1patchOp of obj.Patch.Operation) {
+        if (lvl1patchOp['$'].Class === 'PatchOperationFindMod') {
+            lvl1patchOp.mods[0].li.forEach(item => result.add(item.trim()));
+        } else if (lvl1patchOp['$'].Class === 'PatchOperationSequence') {
+            for (const patchOp of lvl1patchOp.operations[0].li) {
+                if (patchOp['$'].Class === 'PatchOperationFindMod') {
+                    patchOp.mods[0].li.forEach(item => result.add(item.trim()));
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+async function writeModsIndex(content, outFile) {
+    await fs.writeFile(
+        outFile,
+        JSON.stringify(content, (_, value) => value instanceof Set ? [...value] : value, 2)
+    );
+
+    console.log(`Mods index file is located at ${outFile}`);
+}
+
+await main();
