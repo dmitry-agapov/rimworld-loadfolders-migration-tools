06e0d203da825935d5cc49dfa7d93162cc5dc24f
Dmitry Agapov <dmitryagapov89@gmail.com>
2024-03-26 04:15:24 +0300

    scan-mods: Refactoring
    - Added progress logging
    - Made async

--- src/scan-mods.ts
+++ src/scan-mods.ts
@@ -1,4 +1,4 @@
-import fs from 'node:fs';
+import fs from 'node:fs/promises';
 import path from 'node:path';
 import * as commander from 'commander';
 import * as utils from './utils.js';
@@ -10,19 +10,22 @@ commander.program
         'C:\\Program Files (x86)\\Steam\\steamapps\\workshop\\content\\294100',
     )
     .argument('[string]', 'Output file path.', path.resolve('./known-mods.json'))
-    .action((dirPath: string, outFilePath: string) => {
+    .action(async (dirPath: string, outFilePath: string) => {
         dirPath = path.resolve(dirPath);
         outFilePath = path.resolve(outFilePath);
-        const dirContent = fs.readdirSync(dirPath);
+        const dirContent = await fs.readdir(dirPath, 'utf-8');
         const filesToScan = dirContent.map((item) => path.join(dirPath, item, 'about/about.xml'));
         const knownMods = new utils.KnownMods({
             Royalty: ['Ludeon.Rimworld.Royalty'],
             Ideology: ['Ludeon.Rimworld.Ideology'],
             Biotech: ['Ludeon.Rimworld.Biotech'],
         });
+        const logProgress = utils.createProgressLogger('Scanning', filesToScan.length);
 
         for (const filePath of filesToScan) {
-            const file = fs.readFileSync(filePath, 'utf-8');
+            logProgress(`...${filePath.replace(dirPath, '')}`);
+
+            const file = await fs.readFile(filePath, 'utf-8');
             const { name, packageId } = utils.extractModMetadata(file);
 
             if (name && packageId) knownMods.add(name, packageId);
@@ -30,6 +33,8 @@ commander.program
 
         const outFile = utils.fixEOL(JSON.stringify(knownMods));
 
-        fs.writeFileSync(outFilePath, outFile, 'utf-8');
+        await fs.writeFile(outFilePath, outFile, 'utf-8');
+
+        console.log('Done!');
     })
-    .parse();
+    .parseAsync();
